0c0341ea1613546b248fdcbcf23f21a12d699134
khanian <khanylee@gmail.com>
2023-02-04 11:47:46 +0900

    add test 1

---
+++ src/main/java/org/example/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package org.example.calculator;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+        return operand1 + operand2;
+    }
+}
