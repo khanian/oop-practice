349b5f1b7476a9a65b4fdfd72c53474a3b23dfa0
khanian <khanylee@gmail.com>
2023-02-07 07:20:01 +0900

    refactoring calculate by positive number test

--- src/test/java/org/example/calculator/PositiveNumberTest.java
+++ src/test/java/org/example/calculator/PositiveNumberTest.java
@@ -1,15 +1,17 @@
 package org.example.calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class PositiveNumberTest {
     @DisplayName("0또는 음수를 전달할 수 없습니다.")
-    @Test
-    void creatTest() {
-        assertThatCode(() -> new PositiveNumber(0))
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void creatTest(int value) {
+        assertThatCode(() -> new PositiveNumber(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("0또는 음수를 전달할 수 없습니다.");
     }
