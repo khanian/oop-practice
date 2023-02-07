b8fe1f42a715ef0782119656214a63ec04c77557
khanian <khanylee@gmail.com>
2023-02-08 04:30:22 +0900

    refactoring to use 일급 컬렉션

--- src/main/java/org/example/grade/Courses.java
+++ src/main/java/org/example/grade/Courses.java
@@ -3,7 +3,7 @@ package org.example.grade;
 import java.util.List;
 
 public class Courses {
-    // 일급 콜렉션으로 변경
+    // 일급 콜렉션으로 변경 : 하나의 List 나 Set 만 가진 클래스. 관련 로직만 가짐.
     private final List<Course> courses;
 
     public Courses(List<Course> courses) {
@@ -11,11 +11,15 @@ public class Courses {
     }
 
     public double multiplyCreditAndCourseGrade() {
-        double multipliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
-        return multipliedCreditAndCourseGrade;
+        return courses.stream()
+                .mapToDouble(Course::multiplyCreditAndCourseGrade)
+                .sum();
+        // refactoring
+//        double multipliedCreditAndCourseGrade = 0;
+//        for (Course course : courses) {
+//            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
+//        }
+//        return multipliedCreditAndCourseGrade;
     }
 
     public int calculateTotalCompletedCredit() {
