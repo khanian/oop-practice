f34d4be1b8fa6f0dea01c64ce18adb50c35a92c5
khanian <khanylee@gmail.com>
2023-02-08 04:37:52 +0900

    modify 주석

--- src/main/java/org/example/grade/GradeCalculator.java
+++ src/main/java/org/example/grade/GradeCalculator.java
@@ -14,11 +14,10 @@ public class GradeCalculator {
     }
 
     /**
-     * 요구사항
-     * 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
-     * MVC패턴(Model-View-Controller) 기반으로 구현한다.
-     * 일급 컬렉션 사용
+     * 핵심 포인트
      */
+    // 이수한 과목을 전달하여 평균학점 계산 요청 --> 학점계산기 --> (학점수*교과목 평점)의 합계 --> 과목(코스)
+    //                                            --> 수강신청 총학점 수        --> 과목(코스)
     public double calculateGrade() {
         // (학점수×교과목 평점)의 합계
         double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
