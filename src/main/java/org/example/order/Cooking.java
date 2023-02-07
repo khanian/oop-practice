562e983b22b23d1cda570ac5d48fd204ab071a8c
khanian <khanylee@gmail.com>
2023-02-08 05:37:37 +0900

    add cook order makeCook test

--- src/main/java/org/example/order/Cooking.java
+++ src/main/java/org/example/order/Cooking.java
@@ -2,6 +2,7 @@ package org.example.order;
 
 public class Cooking {
     public Cook makeCook(MenuItem menuItem) {
-        return new Cook("돈까스", 5000);
+        Cook cook = new Cook(menuItem);
+        return cook;
     }
 }
