aa9e53ed820b2d2c83bd419d7c3ae81c22abeaf1
khanian <khanylee@gmail.com>
2023-02-11 10:06:40 +0900

    add choose order manu test

--- src/test/java/org/example/order/CustomerTest.java
+++ src/test/java/org/example/order/CustomerTest.java
@@ -2,11 +2,15 @@ package org.example.order;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 /**
  * 음식점에서 음식 주문하는 과정 구현
  * 요구사항
  * 1. 도메인을 구성하는 객체엔 어떤 것들이 있는지 고민
- *     ㄴ 손님, 메뉴판, 메뉴 & 요리:돈까스/냉면/만두(0), 요리사
+ *     ㄴ 손님(0), 메뉴판(0), 메뉴 & 요리:돈까스/냉면/만두(0), 요리사(0)
  * 2. 객체들 간의 관계 고민
  *     ㄴ 손님 -- 메뉴판
  *     ㄴ 손님 -- 요리사
@@ -22,7 +26,13 @@ import org.junit.jupiter.api.Test;
  */
 public class CustomerTest {
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        Cooking cooking = new Cooking();
+
+        assertThatCode(() -> customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
 
     }
 }
